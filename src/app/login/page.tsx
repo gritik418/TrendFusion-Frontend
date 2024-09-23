ff8aebd49bd6a8cc100c99cf952c49e57689f98f
Ritik <160255700+gritik418@users.noreply.github.com>
2024-09-23 13:55:35 +0530

    Login page ui completed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,84 @@
 import Navbar from "@/components/Navbar/Navbar";
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FaEye } from "react-icons/fa";
 
 const LoginPage = () => {
   return (
-    <div>
+    <div className="">
       <Navbar />
+
+      <div className="container p-3 m-auto mt-16 flex">
+        <div className="hidden md:flex w-1/2 items-center justify-center">
+          <Image
+            className="h-full"
+            src={"/images/login.jpg"}
+            height={400}
+            width={500}
+            alt="login image"
+          />
+        </div>
+
+        <div className="w-full md:w-1/2 flex gap-5 flex-col justify-center px-4">
+          <div className="flex flex-col items-center gap-2 mb-5">
+            <Image
+              src={"/images/logo.png"}
+              height={80}
+              width={130}
+              alt="logo"
+            />
+            <h1 className="text-3xl sm:text-5xl font-bold text-[var(--secondary-color)]">
+              Welcome Back
+            </h1>
+            <h2 className="text-lg sm:text-xl text-[var(--primary-color)]">
+              Login to your account
+            </h2>
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label htmlFor="email" className="text-gray-500 text-lg">
+              Enter your Email or Username
+            </label>
+            <input
+              className="bg-slate-100 py-2 px-2 rounded-md outline-[var(--secondary-color)]"
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email or Username"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label htmlFor="password" className="text-gray-500 text-lg">
+              Enter your Password
+            </label>
+            <div className="bg-slate-100 rounded-md relative">
+              <input
+                className="h-full w-full py-2 px-2 bg-transparent outline-[var(--secondary-color)] rounded-md"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+              />
+              <FaEye className="cursor-pointer absolute top-[50%] right-2 -translate-y-[50%] text-[var(--primary-color)] text-lg" />
+            </div>
+          </div>
+
+          <button className="bg-[var(--secondary-color)] mt-6 text-white font-bold text-xl py-2 rounded-md">
+            Login
+          </button>
+
+          <p className="text-center">
+            Don&apos;t have an account?{" "}
+            <Link
+              href="/signup"
+              className="text-lg font-bold text-[var(--secondary-color)]"
+            >
+              Create an account
+            </Link>{" "}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import { IoCartOutline } from "react-icons/io5";
 import { CiLogin } from "react-icons/ci";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <>
-      <div className="h-[60px] shadow-lg hidden sm:flex">
+      <div className="h-[60px] shadow-lg hidden sm:flex bg-white/75 backdrop-blur-md sticky">
         <div className="h-full container m-auto gap-6 flex items-center">
-          <h1>TrendFusion</h1>
+          <Link
+            href={"/"}
+            className="flex text-[var(--secondary-color)] text-xl font-bold"
+          >
+            <Image
+              src={"/images/logo.png"}
+              height={70}
+              width={140}
+              alt="logo"
+            />
+          </Link>
           <SearchBar />
 
           <button className="flex items-center text-lg gap-1 hover:bg-slate-50 transition-colors ease-in-out duration-300 py-1 px-3 rounded-md">
@@ -20,9 +32,19 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="h-[100px] shadow-lg flex-col flex sm:hidden">
+      <div className="h-[100px] shadow-lg flex-col flex sm:hidden bg-white/75 sticky">
         <div className="h-full px-3 container m-auto gap-6 justify-between flex items-center">
-          <h1>TrendFusion</h1>
+          <Link
+            href={"/"}
+            className="flex text-[var(--secondary-color)] text-xl font-bold"
+          >
+            <Image
+              src={"/images/logo.png"}
+              height={70}
+              width={110}
+              alt="logo"
+            />
+          </Link>
 
           <div className="flex gap-4">
             <button className="flex items-center text-lg gap-1 hover:bg-slate-50 transition-colors ease-in-out duration-300 py-1 px-3 rounded-md">
